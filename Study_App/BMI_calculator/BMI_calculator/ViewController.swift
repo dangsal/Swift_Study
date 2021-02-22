d022eae94e54bcb96b9602691a77a0e1cb3669e1
dangsal <sholee0508@naver.com>
2021-02-22 22:20:34 +0900

    outlet action connect

--- Study_App/BMI_calculator/BMI_calculator/ViewController.swift
+++ Study_App/BMI_calculator/BMI_calculator/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtHeight: UITextField!
+    @IBOutlet var txtWeight: UITextField!
+    @IBOutlet var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnResult(_ sender: UIButton) {
+    }
+    
 }
 
