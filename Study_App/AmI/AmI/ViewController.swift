6544c620a8415ec8802211704fdd759a820a6fe8
dangsal <sholee0508@naver.com>
2021-02-22 15:27:29 +0900

    textField 랑 label 연결

--- Study_App/AmI/AmI/ViewController.swift
+++ Study_App/AmI/AmI/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblHello: UILabel! // 초기값이 없어서 옵셔널!
+    
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: UIButton) {
+        
+        lblHello.text = "Hello, " + txtName.text!
+        
+    }
+    
 }
 
