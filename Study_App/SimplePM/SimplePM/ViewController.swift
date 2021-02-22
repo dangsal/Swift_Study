7656b241ac9b0b056598dcd602b0e17a98a55330
dangsal <sholee0508@naver.com>
2021-02-22 18:00:54 +0900

    simplePM

--- Study_App/SimplePM/SimplePM/ViewController.swift
+++ Study_App/SimplePM/SimplePM/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblNumber: UILabel!
+    
+    var x = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func btnPlus(_ sender: UIButton) {
+        x += 1
+        lblNumber.text = String(x)
+    }
+    
+    @IBAction func btnMinus(_ sender: UIButton) {
+        x -= 1
+        lblNumber.text = String(x)
+    }
+    
 }
 
