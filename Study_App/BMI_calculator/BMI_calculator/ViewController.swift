30e168e3e7e25a233d9403da9ac4447ee127eced
dangsal <sholee0508@naver.com>
2021-02-22 19:18:10 +0900

    init commit

---
+++ Study_App/BMI_calculator/BMI_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_calculator
+//
+//  Created by 이성호 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
