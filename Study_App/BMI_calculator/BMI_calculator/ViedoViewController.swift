3810cf84d33ab555c51b42167582c9c442011208
dangsal <sholee0508@naver.com>
2021-02-23 15:37:44 +0900

    app 에 video 추가

--- Study_App/BMI_calculator/BMI_calculator/ViedoViewController.swift
+++ Study_App/BMI_calculator/BMI_calculator/ViedoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ViedoViewController: UIViewController {
 
@@ -15,4 +16,14 @@ class ViedoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func playVideo(_ sender: UIButton) {
+        let file:String? = Bundle.main.path(forResource: "IlikeYou", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: file!)
+        let playerController = AVPlayerViewController()
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        self.present(playerController, animated: true)
+        player.play()
+    }
+      
 }
