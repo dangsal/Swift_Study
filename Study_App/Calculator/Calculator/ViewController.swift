d8ec233e5d739cedd5091e6ee4757d425c3bb10d
dangsal <sholee0508@naver.com>
2021-02-23 17:51:08 +0900

    init commit

---
+++ Study_App/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 이성호 on 2021/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblResult: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func btn0(_ sender: UIButton) {
+    }
+    @IBAction func btn1(_ sender: UIButton) {
+    }
+    @IBAction func btn2(_ sender: UIButton) {
+    }
+    @IBAction func btn3(_ sender: UIButton) {
+    }
+    @IBAction func btn4(_ sender: UIButton) {
+    }
+    @IBAction func btn5(_ sender: UIButton) {
+    }
+    @IBAction func btn6(_ sender: UIButton) {
+    }
+    @IBAction func btn7(_ sender: UIButton) {
+    }
+    @IBAction func btn8(_ sender: UIButton) {
+    }
+    @IBAction func btn9(_ sender: UIButton) {
+    }
+    @IBAction func btnDot(_ sender: UIButton) {
+    }
+    @IBAction func btnResult(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPlus(_ sender: UIButton) {
+    }
+    @IBAction func btnMinus(_ sender: UIButton) {
+    }
+    @IBAction func btnMulti(_ sender: UIButton) {
+    }
+    @IBAction func btnDivide(_ sender: UIButton) {
+    }
+    @IBAction func btnPercent(_ sender: UIButton) {
+    }
+    @IBAction func btnAbs(_ sender: UIButton) {
+    }
+    @IBAction func btnReset(_ sender: UIButton) {
+    }
+    
+}
+
