a44c54e59ed58ae21cb5316064f80fe4999c324d
dangsal <sholee0508@naver.com>
2021-02-23 16:10:32 +0900

    웹뷰 코드 주석 추가

--- Study_App/BMI_calculator/BMI_calculator/WebViewController.swift
+++ Study_App/BMI_calculator/BMI_calculator/WebViewController.swift
@@ -21,9 +21,9 @@ class WebViewController: UIViewController {
 
     @IBAction func btnGoNaver(_ sender: UIButton) {
         
-        guard let url = URL(string: "https://m.naver.com") else {return}
-        let request = URLRequest(url: url)
-        webView.load(request)
+        guard let url = URL(string: "https://m.naver.com") else {return} // guard <불리한표현식> else { 거짓일때 실행될코드, 빠져나가는 코드 }
+        let request = URLRequest(url: url)                              // 표현식이 참일 경우에 실행되는 코드는 이곳에 위치
+        webView?.load(request)
         
         
     }
