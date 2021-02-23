749ff5bc9476122a5aa513b2b1cfc0509158dca9
dangsal <sholee0508@naver.com>
2021-02-23 14:53:37 +0900

    VideoViewController 추가

---
+++ Study_App/BMI_calculator/BMI_calculator/ViedoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViedoViewController.swift
+//  BMI_calculator
+//
+//  Created by 이성호 on 2021/02/23.
+//
+
+import UIKit
+
+class ViedoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
