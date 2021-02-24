ac3422a9455d4d2d7e16e71f2796a962c67a47a0
dangsal <sholee0508@naver.com>
2021-02-24 20:27:34 +0900

    단항연산 추가

--- Study_App/Calculator/Calculator/CalculatorBrain.swift
+++ Study_App/Calculator/Calculator/CalculatorBrain.swift
@@ -14,16 +14,31 @@ class CalculatorBrain {
         accumulator = operand
     }
     
-    private var operations: Dictionary<String,Double> = [
-        "π" : Double.pi,
-        "e" : M_E
+    private var operations: Dictionary<String,Operation> = [
+        "π" : Operation.Constant(Double.pi),   //Double.pi,
+        "e" : Operation.Constant(M_E),     //M_E,
+        "√" : Operation.UnaryOperation(sqrt) ,    //sqrt,
+        "cos" : Operation.UnaryOperation(cos)    //cos
         
     ]
     
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation( (Double) -> Double )
+        case BinaryOperation
+        case Equals
+    }
     
     func performOperation(symbol : String) {
-        if let constant = operations[symbol]{
-            accumulator = constant
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value): accumulator = value
+            case .UnaryOperation(let function) : accumulator = function(accumulator)
+            case .BinaryOperation: break
+            case .Equals: break
+            default: break
+                
+            }
         }
     }
     
