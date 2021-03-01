a36253767f622f3ff41ca8a4a2da3b7fe7b5aac3
dangsal <sholee0508@naver.com>
2021-03-01 16:50:50 +0900

    center 잡기

--- Study_App/FaceIt/FaceIt/FaceView.swift
+++ Study_App/FaceIt/FaceIt/FaceView.swift
@@ -12,6 +12,12 @@ class FaceView: UIView {
 
     override func draw(_ rect: CGRect) {
         
+
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullCenter = CGPoint(x:bounds.midX, bounds.minY)
+        
+        let skull = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2*Double.pi), clockwise: false)
+        
     }
 
 
