eedcea1db46dd957ceb109bb0ec589db5666f82f
dangsal <sholee0508@naver.com>
2021-03-01 16:43:04 +0900

    init commit

---
+++ Study_App/FaceIt/FaceIt/FaceView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by 이성호 on 2021/03/01.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+
+}
---
+++ Study_App/FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by 이성호 on 2021/03/01.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+
+}
+
