7d1b3979bbb7a5a701f73d6f2964953f1df36bb3
dangsal <sholee0508@naver.com>
2021-03-09 19:19:22 +0900

    카카오톡 프로필 UI AutoLayout 적용 클론

--- Study_App/KaKaoTalk_Profile_Page_AutoLayout/KaKaoTalk_Profile_Page_AutoLayout/ViewController.swift
+++ Study_App/KaKaoTalk_Profile_Page_AutoLayout/KaKaoTalk_Profile_Page_AutoLayout/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet var emailButton: UIButton!
+    
+    
+    
+    @IBOutlet var profilImageView: UIImageView!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        emailButton.layer.cornerRadius = 3
+        emailButton.layer.borderColor = UIColor.lightGray.cgColor
+        emailButton.layer.borderWidth = 1
+        
+        profilImageView.layer.cornerRadius = profilImageView.bounds.width / 2
     }
 
 
