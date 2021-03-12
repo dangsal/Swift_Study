940fab8c99a90c4629b75ce4ccb3cdc81bafc2ce
dangsal <sholee0508@naver.com>
2021-03-12 21:32:31 +0900

    init commit

---
+++ Study_App/FirstCoreData/FirstCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstCoreData
+//
+//  Created by 이성호 on 2021/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
