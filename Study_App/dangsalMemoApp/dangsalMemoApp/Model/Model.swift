2e3ff742f2df3f0ee9bee3d9eab0c94f4873a499
dangsal <sholee0508@naver.com>
2021-03-15 17:49:31 +0900

    테이블 뷰에 더미 메모 가져오기

---
+++ Study_App/dangsalMemoApp/dangsalMemoApp/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  dangsalMemoApp
+//
+//  Created by 이성호 on 2021/03/15.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content:String){
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Hello World"),
+        Memo(content: "IOS Memo app ♥️")
+    ]
+}
