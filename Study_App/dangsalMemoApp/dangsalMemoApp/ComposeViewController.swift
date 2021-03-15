a62fe44e280e21a2b373f01cdf8a9a9a2a9cdd4e
dangsal <sholee0508@naver.com>
2021-03-15 19:49:17 +0900

    취소 버튼 활성화ㅣ

--- Study_App/dangsalMemoApp/dangsalMemoApp/ComposeViewController.swift
+++ Study_App/dangsalMemoApp/dangsalMemoApp/ComposeViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+
+    
+    @IBAction func close(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil) // modal 화면 끌때 사용하는 메소드 ,<#T##(() -> Void)?##(() -> Void)?##() -> Void#> 화면이 닫을때 실행할 코드를 클로져로 
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
     /*
     // MARK: - Navigation
 
