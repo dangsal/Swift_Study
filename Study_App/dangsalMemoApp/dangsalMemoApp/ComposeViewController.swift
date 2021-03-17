d31ecc46986ea4fe995cbbb7beeef907949dc7e8
dangsal <sholee0508@naver.com>
2021-03-17 18:15:15 +0900

    메모 편집 알림 오류

--- Study_App/dangsalMemoApp/dangsalMemoApp/ComposeViewController.swift
+++ Study_App/dangsalMemoApp/dangsalMemoApp/ComposeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ComposeViewController: UIViewController {
     
     var editTarget: Memo?
+    var originalMemoContent: String?
 
     @IBOutlet var memoTextView: UITextView!
     
@@ -50,11 +51,28 @@ class ComposeViewController: UIViewController {
         if let memo = editTarget {
             navigationItem.title = "메모 편집"
             memoTextView.text = memo.content
+            originalMemoContent = memo.content
         } else {
             navigationItem.title = "새 메모"
             memoTextView.text = ""
         }
+        
+        memoTextView.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+//        navigationController?.presentationController?.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.presentationController?.delegate = nil
+    }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -66,6 +84,34 @@ class ComposeViewController: UIViewController {
     */
 
 }
+
+extension ComposeViewController :UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        if let original = originalMemoContent, let edited = textView.text {
+            if #available(iOS 13.0, *) {
+                isModalInPresentation = original != edited
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+}
+
+//
+//extension ComposeViewController: UIAdaptivePresentationControllerDelegate{
+//    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+//        let alert = UIAlertController(title:"알림",message: "편집한 내용을 저장할까요?",preferredStyle: .alert)
+//
+//        let okAction = UIAlertAction(title: "확인", style: .default) { [weak self](action) in self?.save(act)}
+//        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { [weak self](action) in self?.close()}
+//        alert.addAction(okAction)
+//        alert.addAction(cancelAction)
+//
+//        present(alert, animated: true, completion: nil)
+//    }
+//
+//}
+
 
 extension ComposeViewController {
     static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
