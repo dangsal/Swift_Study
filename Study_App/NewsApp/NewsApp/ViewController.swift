fd8a087012edf4d598dcf24a777d2fff2b708169
dangsal <sholee0508@naver.com>
2021-04-07 14:39:09 +0900

    init commit

---
+++ Study_App/NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 이성호 on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
