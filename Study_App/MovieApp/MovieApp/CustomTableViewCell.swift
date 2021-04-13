623b04c41ceb2105a4c856f798e1436270dc5153
dangsal <sholee0508@naver.com>
2021-04-13 16:22:23 +0900

    커스텀 셀 UI 까지

---
+++ Study_App/MovieApp/MovieApp/CustomTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  CustomTableViewCell.swift
+//  MovieApp
+//
+//  Created by 이성호 on 2021/04/13.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    
+    
+    
+    var movieDisplay: Movie?
+    
+    func displayMovie(movie:Movie){
+        
+        
+        
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Study_App/MovieApp/MovieApp/ViewController.swift
+++ Study_App/MovieApp/MovieApp/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var tableView: UITableView!
+    
     var model = MovieModel()
     var movie = [Movie]()
 
@@ -27,5 +29,25 @@ extension ViewController: MovieModelProtocol{
     func movieRetrieved(movies: [Movie]) {
         print("movies를 model 에서 가져옴")
         self.movie = movies
+        tableView.reloadData()
     }
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movie.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
