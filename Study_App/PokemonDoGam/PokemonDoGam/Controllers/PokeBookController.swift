7a639f060d576d5f07ca6fd817e6f797e9ec529a
dangsal <sholee0508@naver.com>
2021-05-03 13:59:14 +0900

    init commit

---
+++ Study_App/PokemonDoGam/PokemonDoGam/Controllers/PokeBookController.swift
@@ -0,0 +1,16 @@
+//
+//  PokeBookController.swift
+//  PokemonDoGam
+//
+//  Created by 이성호 on 2021/05/03.
+//
+
+import UIKit
+
+class PokeBookController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World!")
+    }
+}
---
+++ Study_App/PokemonDoGam/PokemonDoGam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonDoGam
+//
+//  Created by 이성호 on 2021/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
