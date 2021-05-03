d823b6ec8ebbed25e696ecc2c2954c962964668f
dangsal <sholee0508@naver.com>
2021-05-03 21:35:34 +0900

    프로그래머틱하게 코딩해서 컴포넌트들 추가

--- Study_App/PokemonDoGam/PokemonDoGam/Views/PokemonCell.swift
+++ Study_App/PokemonDoGam/PokemonDoGam/Views/PokemonCell.swift
@@ -8,6 +8,36 @@
 import UIKit
 
 class PokemonCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    lazy var imageView:UIImageView = {
+       let iv = UIImageView()
+        iv.backgroundColor = UIColor.tertiarySystemGroupedBackground
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    lazy var textView:UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.mainColor
+        view.addSubview(self.pokemonName)
+        pokemonName.translatesAutoresizingMaskIntoConstraints = false
+        pokemonName.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        pokemonName.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        return view
+    }()
+    
+    lazy var pokemonName: UILabel = {
+        let label = UILabel()
+        label.text = "dangsal"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -21,6 +51,22 @@ class PokemonCell: UICollectionViewCell {
     
     // MARK: Helpers
     func configureViewComponents(){
-        self.backgroundColor = .tertiarySystemGroupedBackground
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+        
+        
+        self.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.7).isActive = true
+        
+        self.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        textView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
 }
