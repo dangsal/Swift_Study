9aadf73735c1a9bbe6d31e490fe483d1483c73fe
dangsal <sholee0508@naver.com>
2021-05-12 21:26:50 +0900

    기본 테이블뷰 생성

--- IOS_Academy/CustomSwipeAction/CustomSwipeAction/ViewController.swift
+++ IOS_Academy/CustomSwipeAction/CustomSwipeAction/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        title = "Swipe Actions"
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
 
 }
 
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+}
