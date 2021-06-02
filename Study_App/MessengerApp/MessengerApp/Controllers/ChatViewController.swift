5254fcf961f7df39aff00ede612f0f45d1cd8a9c
dangsal <sholee0508@naver.com>
2021-06-02 11:24:57 +0900

    ChatViewController 구성

--- Study_App/MessengerApp/MessengerApp/Controllers/ChatViewController.swift
+++ Study_App/MessengerApp/MessengerApp/Controllers/ChatViewController.swift
@@ -7,8 +7,28 @@
 
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
+
+struct Sender: SenderType{
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+}
+
+class ChatViewController: MessagesViewController {
+    
+    //MARK: Property
+    
+    var messages = [Message]()
+    
+    let selfSender = Sender(photoURL: "", senderId: "1", displayName: "Joe Smith")
     
     //MARK: Init
     override func viewDidLoad() {
@@ -18,6 +38,30 @@ class ChatViewController: UIViewController {
     
     //MARK: Configure
     func configure(){
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("Hello world message")))
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("♥️")))
     }
+}
+    
+
+extension ChatViewController : MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    
 }
