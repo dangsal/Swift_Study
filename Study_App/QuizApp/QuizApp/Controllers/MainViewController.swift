80f0e763eee9fa62b3896994235f9fd113854905
dangsal <sholee0508@naver.com>
2021-07-19 15:13:07 +0900

    MainController 구성

--- Study_App/QuizApp/QuizApp/Controllers/MainViewController.swift
+++ Study_App/QuizApp/QuizApp/Controllers/MainViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class MainViewController: UIViewController {
     //MARK: Property
+    lazy var startBtn: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Start", for: .normal)
+        bt.tintColor = .white
+        bt.backgroundColor = .black
+        return bt
+    }()
+    
     //MARK: Selectors
     //MARK: Functions
     //MARK: Init
@@ -19,11 +27,20 @@ class MainViewController: UIViewController {
     }
     //MARK: Configure
         func configure(){
-            view.backgroundColor = .systemGray
+            view.backgroundColor = .systemPurple
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationItem.title = "Quiz Game"
+            navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+            navigationController?.navigationBar.largeTitleTextAttributes = [. foregroundColor: UIColor.white]
         }
         
         func configureComponent(){
-            
+            view.addSubview(startBtn)
+            startBtn.translatesAutoresizingMaskIntoConstraints = false
+            startBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            startBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100).isActive = true
+            startBtn.widthAnchor.constraint(equalToConstant: 300).isActive = true
+            startBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
         }
 }
 //MARK: Extensions
