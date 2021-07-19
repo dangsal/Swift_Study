51cbc1ef09ba32910f587c0ee0fd99167fdda970
dangsal <sholee0508@naver.com>
2021-07-19 14:03:27 +0900

    init commit

---
+++ Study_App/RandomPhotoApp/RandomPhotoApp/Controllers/RandomPhotoController.swift
@@ -0,0 +1,24 @@
+//
+//  RandomPhotoController.swift
+//  RandomPhotoApp
+//
+//  Created by 이성호 on 2021/07/19.
+//
+
+import UIKit
+
+class RandomPhotoController : UIViewController {
+    //MARK: Property
+    //MARK: Selectors
+    //MARK: Functions
+    //MARK: Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    //MARK: Configure
+    func configure(){
+        
+    }
+    //MARK: Extensions
+}
