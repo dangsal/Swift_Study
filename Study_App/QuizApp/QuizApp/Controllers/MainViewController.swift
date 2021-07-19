056b102e4d3374808260167ab33393d640a58d0c
dangsal <sholee0508@naver.com>
2021-07-19 14:40:26 +0900

    init commit

---
+++ Study_App/QuizApp/QuizApp/Controllers/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  QuizApp
+//
+//  Created by 이성호 on 2021/07/19.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    //MARK: Property
+    //MARK: Selectors
+    //MARK: Functions
+    //MARK: Init
+    //MARK: Configure
+        func configure(){
+            
+        }
+        
+        func configureComponent(){
+            
+        }
+}
+//MARK: Extensions
