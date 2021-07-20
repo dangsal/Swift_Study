3b95f109a17346fa21cb71e8a7bb27893184431f
dangsal <sholee0508@naver.com>
2021-07-20 17:47:49 +0900

    프로토콜 완벽이해

--- Study_App/CuntDownApp/CuntDownApp/Controllers/CountDownController.swift
+++ Study_App/CuntDownApp/CuntDownApp/Controllers/CountDownController.swift
@@ -7,10 +7,23 @@
 
 import UIKit
 
+
 class CountDownController : UIViewController {
+
     //MARK: Property
+    let nextVC = DatePickController()
+    
+    lazy var label : UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        return label
+    }()
     //MARK: Selectors
+    @objc func addTapped(){
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
     //MARK: Functions
+
     //MARK: Init
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +32,28 @@ class CountDownController : UIViewController {
     }
     //MARK: Configure
         func configure(){
+            view.backgroundColor = .systemGray
+
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
             
+            nextVC.delegate = self
         }
         
         func configureComponent(){
             
+            view.addSubview(label)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            label.widthAnchor.constraint(equalToConstant: 100).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 60).isActive = true
+
         }
 
 }
 //MARK: Extensions
+extension CountDownController: DatePickControllerProtocol {
+    func dataSave(text: String) {
+        label.text = text
+    }
+}
---
+++ Study_App/CuntDownApp/CuntDownApp/Controllers/DatePickController.swift
@@ -0,0 +1,90 @@
+//
+//  DatePickController.swift
+//  CuntDownApp
+//
+//  Created by 이성호 on 2021/07/20.
+//
+
+import UIKit
+
+protocol DatePickControllerProtocol {
+    func dataSave(text : String)
+}
+
+class DatePickController : UIViewController {
+    
+    var delegate: DatePickControllerProtocol?
+
+    //MARK: Property
+    lazy var saveBtn : UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Save", for: .normal)
+        bt.backgroundColor = .white
+        bt.tintColor = .black
+        bt.addTarget(self, action: #selector(saveBtnTapped), for: .touchUpInside)
+        return bt
+    }()
+    
+    lazy var textField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Event Name..."
+        tf.backgroundColor = .white
+        tf.becomeFirstResponder()
+        return tf
+    }()
+    
+    lazy var datePicker : UIDatePicker = {
+        let dp = UIDatePicker(frame: CGRect(x: 0, y: 0, width: view.frame.width - 20, height: 200))
+        dp.preferredDatePickerStyle = .wheels
+        dp.locale = Locale(identifier: "ko-KR")
+        dp.timeZone = NSTimeZone.local
+        dp.backgroundColor = .white
+        dp.layer.cornerRadius = 5
+        return dp
+    }()
+    //MARK: Selectors
+    @objc func saveBtnTapped(){
+        print(datePicker.date)
+        if let text = textField.text, !text.isEmpty {
+            delegate?.dataSave(text: text)
+        }
+        self.navigationController?.popViewController(animated: true)
+    }
+    //MARK: Functions
+    //MARK: Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        configureComponent()
+    }
+    //MARK: Configure
+        func configure(){
+            view.backgroundColor = .systemGray
+            navigationItem.title = "New Event"
+        }
+        
+        func configureComponent(){
+            
+            view.addSubview(textField)
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            textField.widthAnchor.constraint(equalToConstant: view.frame.width - 20).isActive = true
+            textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+            
+            view.addSubview(datePicker)
+            datePicker.translatesAutoresizingMaskIntoConstraints = false
+            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            
+            view.addSubview(saveBtn)
+            saveBtn.translatesAutoresizingMaskIntoConstraints = false
+            saveBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            saveBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+            saveBtn.widthAnchor.constraint(equalToConstant: view.frame.width - 20).isActive = true
+            saveBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        }
+
+}
+//MARK: Extensions
+
