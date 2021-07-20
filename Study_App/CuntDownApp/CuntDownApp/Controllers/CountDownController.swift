3f5047a184a5d5481fae4c417ae441d1ef5301ab
dangsal <sholee0508@naver.com>
2021-07-20 13:27:27 +0900

    init commit

---
+++ Study_App/CuntDownApp/CuntDownApp/Controllers/CountDownController.swift
@@ -0,0 +1,30 @@
+//
+//  CountDownController.swift
+//  CuntDownApp
+//
+//  Created by 이성호 on 2021/07/20.
+//
+
+import UIKit
+
+class CountDownController : UIViewController {
+    //MARK: Property
+    //MARK: Selectors
+    //MARK: Functions
+    //MARK: Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        configureComponent()
+    }
+    //MARK: Configure
+        func configure(){
+            
+        }
+        
+        func configureComponent(){
+            
+        }
+
+}
+//MARK: Extensions
