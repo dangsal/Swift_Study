f7c9f77739b47b7e95637acb93ebc6840589031c
dangsal <sholee0508@naver.com>
2021-07-21 15:28:47 +0900

    init commit

---
+++ Study_App/NotesApp/NotesApp/Controllers/NotesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesViewController.swift
+//  NotesApp
+//
+//  Created by 이성호 on 2021/07/21.
+//
+
+import UIKit
+
+class NotesViewController : UIViewController {
+    //MARK: Property
+    //MARK: Selectors
+    //MARK: Functions
+    //MARK: Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        configureComponent()
+    }
+    //MARK: Configure
+        func configure(){
+            view.backgroundColor = .systemBackground
+        }
+        
+        func configureComponent(){
+            
+        }
+
+}
+//MARK: Extensions
